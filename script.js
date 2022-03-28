8755ecbc1a6d504a4c5790f29043da482db33834
talha <tkz9696@gmail.com>
2022-03-28 20:23:56 +0500

    Basic skeleton and linters added

---
+++ script.js
@@ -0,0 +1,10 @@
+const booksDiv = document.querySelector('booksDiv');
+
+const bookInfo = document.createElement('div');
+
+bookInfo.innerText = "hello";
+
+booksDiv.appendChild(li);
+
+console.log(li);
+console.log(booksDiv);
